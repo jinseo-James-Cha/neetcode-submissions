75940bd71711537fda0e45a39e3739a46cd5658d
Jinseo-James-Cha <78688891+jinseo-James-Cha@users.noreply.github.com>
2026-07-01 12:15:43 +0100

    Add: top-k-elements-in-list - submission-3

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-3.py
@@ -0,0 +1,60 @@
+from collections import defaultdict
+import heapq
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        """
+        most frequent elements
+
+        count how many each num in the list
+        sorting them based on frequence
+        and return by the most frequence
+        """
+
+        # bucket sort
+        # O(n)
+        # O(n)
+        count = defaultdict(int)
+        for num in nums:
+            count[num] += 1
+        
+        bucket = [[] for i in range(len(nums) + 1)]
+        for num, freq in count.items():
+            bucket[freq].append(num)
+        
+        res = []
+        for i in range(len(bucket) - 1, 0, -1):
+            res.extend(bucket[i])
+            if len(res) >= k:
+                break
+        return res[:k]
+
+        # max heap
+        # time: O(n log k)
+        # space: O(n + k)
+        count = defaultdict(int)
+        for num in nums:
+            count[num] += 1
+
+        freq_arr = []
+        for num, freq in count.items():
+            heapq.heappush(freq_arr, [-freq, num])
+        
+        res = []
+        while k > 0:
+            res.append(heapq.heappop(freq_arr)[1])
+            k -= 1
+        return res
+
+        # sorting
+        # time: O(n + n log n + k)
+        count = defaultdict(int)
+        for num in nums:
+            count[num] += 1
+        
+        freq_arr = []
+        for num, freq in count.items():
+            freq_arr.append([freq, num])
+        
+        freq_arr.sort(reverse=True)
+        return [ num for freq, num in freq_arr[:k]]
+        
