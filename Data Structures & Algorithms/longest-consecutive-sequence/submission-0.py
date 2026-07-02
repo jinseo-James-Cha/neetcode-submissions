ba276a09f30fa039c2e39df9240e5f91a74a3f76
Jinseo-James-Cha <78688891+jinseo-James-Cha@users.noreply.github.com>
2026-07-02 14:43:33 +0100

    Add: longest-consecutive-sequence - submission-0

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-0.py
@@ -0,0 +1,23 @@
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+        # bucket
+
+        if not nums:
+            return 0
+
+        max_num = max(nums)
+        min_num = min(nums)
+        buckets = [False] * (max_num - min_num + 2)
+
+        for num in nums:
+            buckets[num - min_num] = True
+        
+        longest = 1
+        current = 0
+        for i in range(len(buckets)):
+            if buckets[i]:
+                current += 1
+            else:
+                longest = max(longest, current)
+                current = 0
+        return longest
