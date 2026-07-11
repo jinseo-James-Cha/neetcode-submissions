1477a456bce5043351c97ed12dc2f6c957c211a0
Jinseo-James-Cha <78688891+jinseo-James-Cha@users.noreply.github.com>
2026-07-12 00:43:42 +0100

    Add: car-fleet - submission-2

---
+++ Data Structures & Algorithms/car-fleet/submission-2.py
@@ -0,0 +1,47 @@
+class Solution:
+    def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
+        """
+        n cars
+
+        - - - - - - - - -
+          C     C
+        
+        1 4
+        3 2
+
+        4 6
+        7 8
+        10 10
+
+        4 1 0 7
+        2 2 1 1
+
+        6 3 1 8
+        8 5 2 9
+        10 7 3 10
+
+        4 + 2 * x >= target
+        2  * x >= target - 4 
+        x >= target - 4 / 2
+        
+        x = 3
+
+        Starting from the postion which near target
+        and calculate x(time)
+        if my time takes longer than behind(ahead position)
+        it is new fleet,
+        otherwise, it catches up an ahead car
+        """
+        cars = sorted(zip(position, speed), reverse=True)
+
+        stack = []
+
+        for p, s in cars:
+            time = (target - p) / s
+
+            if not stack or time > stack[-1]:
+                stack.append(time)
+
+        return len(stack)
+
+
