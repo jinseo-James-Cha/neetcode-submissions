781eb2fb7572fb8af5673bad353d146f86360ad7
Jinseo-James-Cha <78688891+jinseo-James-Cha@users.noreply.github.com>
2026-08-11 15:56:43 +0100

    Add: course-schedule - submission-0

---
+++ Data Structures & Algorithms/course-schedule/submission-0.py
@@ -0,0 +1,37 @@
+from collections import defaultdict
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        """
+        a, b : must a first and then b
+        b -> a
+        """
+
+        # Kahn's algorithm
+        # topological sort
+        graph = defaultdict(list)
+        indegree = [0] * numCourses
+        for a, b in prerequisites:
+            graph[b].append(a)
+            indegree[a] += 1
+        
+        # set start position
+        queue = deque()
+        for course in range(numCourses):
+            if indegree[course] == 0:
+                queue.append(course)
+    
+        while queue:
+            curr_course = queue.popleft()
+            numCourses -= 1
+
+            for next_course in graph[curr_course]:
+                indegree[next_course] -= 1
+
+                if indegree[next_course] == 0:
+                    queue.append(next_course)
+        return numCourses == 0
+
+
+
+
+
