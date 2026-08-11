89794c55879c34d6f54d43ab02a3f15086166cdf
Jinseo-James-Cha <78688891+jinseo-James-Cha@users.noreply.github.com>
2026-08-11 11:57:33 +0100

    Add: surrounded-regions - submission-3

---
+++ Data Structures & Algorithms/surrounded-regions/submission-3.py
@@ -0,0 +1,56 @@
+class Solution:
+    def solve(self, board: List[List[str]]) -> None:
+        """
+        'X':
+        'O':
+
+        connect: horizontally or vertically
+        region: connect every 'O' cell. any shape
+        surround: on the edge of the board
+        """
+        # BFS
+        m, n = len(board), len(board[0])
+
+
+        # DFS
+        m, n = len(board), len(board[0])
+        def is_within_bounds(row, col):
+            return 0 <= row < m and 0 <= col < n
+
+        DIRS = [(1,0), (-1,0), (0,1), (0,-1)]
+        def dfs(row, col):
+            if board[row][col] != "O":
+                return
+            
+            board[row][col] = "T"
+            for dy, dx in DIRS:
+                next_r, next_c = row+dy, col+dx
+                if not is_within_bounds(next_r, next_c):
+                    continue
+                if board[next_r][next_c] != 'O':
+                    continue
+                dfs(next_r, next_c)
+
+
+        # DFS from edge, mark T for O cells from an edge
+        # top and bottom row edges
+        for col in range(n):
+            if board[0][col] == "O":
+                dfs(0, col)
+            if board[m - 1][col] == "O":
+                dfs(m-1, col)
+        
+        # left and right col edges
+        for row in range(m):
+            if board[row][0] == "O":
+                dfs(row, 0)
+            if board[row][n - 1] == "O":
+                dfs(row, n-1)
+            
+        for row in range(m):
+            for col in range(n):
+                if board[row][col] == "O":
+                    board[row][col] = "X"
+                elif board[row][col] == "T":
+                    board[row][col] = "O"
+
