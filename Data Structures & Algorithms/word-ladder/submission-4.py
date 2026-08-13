5182056f336837a14bddae43a6661159bbe18119
Jinseo-James-Cha <78688891+jinseo-James-Cha@users.noreply.github.com>
2026-08-13 12:50:00 +0100

    Add: word-ladder - submission-4

---
+++ Data Structures & Algorithms/word-ladder/submission-4.py
@@ -0,0 +1,48 @@
+class Solution:
+    def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
+        """
+        lowercase alphabets nad all distict
+
+        cat, sag , ["bat","bag","sag","dag","dot"]
+
+        cat -> bat -> bag -> dag -> sag
+                          -> sag
+
+    
+
+        """
+        
+        if len(beginWord) != len(endWord):
+            return 0
+        
+        if endWord not in wordList:
+            return 0
+        
+        if beginWord == endWord:
+            return 1
+
+       
+        wordSet = set(wordList)
+        res = 0
+        
+        queue = deque([beginWord])
+        while queue:
+            res += 1
+            for _ in range(len(queue)):
+                node = queue.popleft()
+                if node == endWord:
+                    return res
+                
+                for i in range(len(node)):
+                    for j in range(26):
+                        new_char = chr(j + ord('a'))
+                        if new_char == node[i]:
+                            continue
+                        new_node = node[:i] + new_char + node[i+1:]
+                        if new_node in wordSet:
+                            queue.append(new_node)
+                            wordSet.remove(new_node)
+        return 0
+
+
+
