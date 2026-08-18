26d564fa3ddb023e40c8be9725e5a5a3862e4f93
Jinseo-James-Cha <78688891+jinseo-James-Cha@users.noreply.github.com>
2026-08-18 11:55:03 +0100

    Add: min-cost-climbing-stairs - submission-0

---
+++ Data Structures & Algorithms/min-cost-climbing-stairs/submission-0.py
@@ -0,0 +1,31 @@
+class Solution:
+    def minCostClimbingStairs(self, cost: List[int]) -> int:
+        """
+        cost[i] = cost of taking a step from ith floor
+        after the i, you can go either i+1 or i+2
+
+        starting point either 0 or 1
+
+        top floor = n + 1
+        n = len(cost)
+
+        0 1 2 3 ... n
+        n에 도착하려면
+        n-1까지의 최솟값 + cost[n-1]
+        n-2까지의 최솟값 + cost[n-2]
+        """
+
+        def dp(idx):
+            if idx <= 1:
+                return 0
+            
+            if idx not in memo:
+                one_back = dp(idx-1) + cost[idx - 1] # 3, dp(2) + cost[2]
+                two_back = dp(idx-2) + cost[idx - 2] # 3, dp(1) + cost[1]
+                memo[idx] = min(one_back, two_back)
+            
+            return memo[idx]
+
+
+        memo = {}
+        return dp(len(cost))
