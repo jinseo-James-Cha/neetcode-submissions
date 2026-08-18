d72f4298d2615ec6028a245bf1676eedec17b9f6
Jinseo-James-Cha <78688891+jinseo-James-Cha@users.noreply.github.com>
2026-08-18 11:34:38 +0100

    Add: climbing-stairs - submission-2

---
+++ Data Structures & Algorithms/climbing-stairs/submission-2.py
@@ -0,0 +1,42 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        """
+        1 step or 2steps at a time
+        reach n stairs        
+
+        1
+
+        1 1
+        2
+
+        1 1 1
+        1 2
+        2 1
+
+        1 1 1 1
+        1 2 1
+        1 1 2
+        2 1 1
+        2 2
+        """
+        # DP - bottom up
+        if n <= 2:
+            return n
+        dp = [0] * (n+1)
+        dp[1] = 1
+        dp[2] = 2
+        for i in range(3, n + 1):
+            dp[i] = dp[i-1] + dp[i-2]
+        return dp[n]
+
+        # DP - top down
+        def dp(n):
+            if n <= 2:
+                return n
+
+            if n not in memo:
+                memo[n] = dp(n-1) + dp(n-2)
+            return memo[n]
+        
+        memo = {}
+        return dp(n)
