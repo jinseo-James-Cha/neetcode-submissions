a9e2e919b5c1460ffb8b41e95be12b42ef71a38d
Jinseo-James-Cha <78688891+jinseo-James-Cha@users.noreply.github.com>
2026-08-23 22:36:42 +0100

    Add: count-paths - submission-1

---
+++ Data Structures & Algorithms/count-paths/submission-1.py
@@ -0,0 +1,33 @@
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        """
+        start 0,0 
+        end m-1, n-1
+
+        can move right or down..
+        00 -> -> ->
+        |  2   3  4
+        |
+        |
+        """
+        # DP - bottom up
+        # dp = [[1] * n for _ in range(m)]
+        # for row in range(1, m):
+        #     for col in range(1, n):
+        #         dp[row][col] = dp[row-1][col] + dp[row][col-1]
+        
+        # return dp[m-1][n-1]
+
+        # DP - top down
+        def dp(row, col):
+            if row == 0 or col == 0:
+                return 1
+            
+            if (row, col) not in memo:
+                memo[(row,col)] = dp(row-1, col) + dp(row, col-1)
+            return memo[(row, col)]
+        memo = {}
+        return dp(m-1, n-1)
+
+
+
