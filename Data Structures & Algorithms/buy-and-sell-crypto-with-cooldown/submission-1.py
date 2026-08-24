604757c098bc74d54ad6ebca99537f82699a1cdb
Jinseo-James-Cha <78688891+jinseo-James-Cha@users.noreply.github.com>
2026-08-24 12:13:42 +0100

    Add: buy-and-sell-crypto-with-cooldown - submission-1

---
+++ Data Structures & Algorithms/buy-and-sell-crypto-with-cooldown/submission-1.py
@@ -0,0 +1,48 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        """
+        sell -> cannot buy one on the next day
+        own at most one
+
+        buy -> sell -> cooldown -> buy...
+        """
+        # DP - bottom up
+        n = len(prices)
+        dp = [[0] * 2 for _ in range(n + 2)]
+        for i in range(n-1, -1, -1):
+            for is_holding in range(2):
+                do_something = 0
+                if is_holding:
+                    do_something = prices[i] + dp[i+2][False]
+                else:
+                    do_something = -prices[i] + dp[i+1][True]
+                
+                do_nothing = dp[i+1][is_holding]
+                dp[i][is_holding] = max(do_something, do_nothing)
+        return dp[0][False]
+
+
+
+
+
+
+
+        # DP - top down
+        def dp(idx, is_holding):
+            if idx >= len(prices):
+                return 0
+            
+            if (idx, is_holding) not in memo:
+                do_something = 0
+                if is_holding:
+                    do_something = prices[idx] + dp(idx + 2, False)
+                else:
+                    do_something = -prices[idx]+dp(idx+1, True)
+                
+                do_nothing = dp(idx+1, is_holding)
+
+                memo[(idx, is_holding)] = max(do_something, do_nothing)
+            return memo[(idx, is_holding)]
+                
+        memo = {}
+        return dp(0, False)
