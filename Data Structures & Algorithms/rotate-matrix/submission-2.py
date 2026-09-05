ce192bcf78015ffc77a6086116adca7e5063e87f
Jinseo-James-Cha <78688891+jinseo-James-Cha@users.noreply.github.com>
2026-09-05 10:10:03 +0900

    Add: rotate-matrix - submission-2

---
+++ Data Structures & Algorithms/rotate-matrix/submission-2.py
@@ -0,0 +1,13 @@
+class Solution:
+    def rotate(self, matrix: List[List[int]]) -> None:
+        # again * 2
+        n = len(matrix)
+        rotated = [[0] * n for _ in range(n)]
+
+        for i in range(n):
+            for j in range(n):
+                rotated[j][n - 1 - i] = matrix[i][j]
+
+        for i in range(n):
+            for j in range(n):
+                matrix[i][j] = rotated[i][j]
