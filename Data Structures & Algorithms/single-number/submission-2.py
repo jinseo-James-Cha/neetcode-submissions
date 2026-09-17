3087ec458fdd7c69626f96ceab23803ac3f145d1
Jinseo-James-Cha <78688891+jinseo-James-Cha@users.noreply.github.com>
2026-09-17 23:15:31 +0900

    Add: single-number - submission-2

---
+++ Data Structures & Algorithms/single-number/submission-2.py
@@ -0,0 +1,28 @@
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        nums.sort()
+        i = 0
+        while i < len(nums) - 1:
+            if nums[i] == nums[i + 1]:
+                i += 2
+            else:
+                return nums[i]
+        return nums[i]
+
+
+        seen = set()
+        for num in nums:
+            if num in seen:
+                seen.remove(num)
+            else:
+                seen.add(num)
+        return list(seen)[0]
+
+        for i in range(len(nums)):
+            flag = True
+            for j in range(len(nums)):
+                if i != j and nums[i] == nums[j]:
+                    flag = False
+                    break
+            if flag:
+                return nums[i]
